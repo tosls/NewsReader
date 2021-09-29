08d3f9ee257bf11f4ec9a7a9209ae8dc91c02f3a
Anton Bobryshev <65329095+tosls@users.noreply.github.com>
2021-09-29 19:44:31 +0700

    created cell view added cell model

--- NewsReader/NetworkManager/NetworkManager.swift
+++ NewsReader/NetworkManager/NetworkManager.swift
@@ -17,7 +17,7 @@ final class NetworkManager {
     
     private init() {}
     
-    public func getTopStories(completion: @escaping (Result<[String], Error>)-> Void) {
+    public func getTopStories(completion: @escaping (Result<[Article], Error>)-> Void) {
         guard let url = Constants.topHeadLinesURL else {return}
         
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
@@ -26,7 +26,8 @@ final class NetworkManager {
             } else if let data = data {
                 do {
                     let result = try JSONDecoder().decode(NewsModel.self, from: data)
-                    print(result.articles.count)
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
                 }
                 catch {
                     completion(.failure(error))
--- NewsReader/ViewControllers/ViewController.swift
+++ NewsReader/ViewControllers/ViewController.swift
@@ -6,16 +6,20 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
     
     private let tableView: UITableView = {
         let table = UITableView()
-        table.register(UITableViewCell.self,
-                       forCellReuseIdentifier: "cell")
+        table.register(NewsTableViewCell.self,
+                       forCellReuseIdentifier: NewsTableViewCell.identifier)
         
         return table
     }()
+    
+    private var articles = [Article]()
+    private var viewModels = [NewTableViewCellViewModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,32 +29,47 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        NetworkManager.shared.getTopStories { result in
-            switch result {
-            case .success(let response):
-                break
-            case .failure(let error):
-                print(error)
-            }
-        }
+        fetchTopStories()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
+    
+    private func fetchTopStories() {
+        NetworkManager.shared.getTopStories { [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewTableViewCellViewModel(title: $0.title,
+                                              subTitle: $0.description ?? "No Description",
+                                              imageURL: URL(string: $0.urlToImage ?? "")
+                                              )
+                })
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
-                                                 for: indexPath)
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
         var content = cell.defaultContentConfiguration()
         content.text = "Test"
 
@@ -59,6 +78,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        guard let url = URL(string: article.url ?? "") else { return }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
 }
 
